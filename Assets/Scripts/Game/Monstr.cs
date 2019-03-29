146c15dfd25067217f41cac0c38c9e7d8c68cfb1
Maksatus <maksim040@mail.ru>
2019-03-29 20:16:48 +0400

    0.12
    Added monsters,additional sounds and other bonuses

---
+++ Assets/Scripts/Game/Monstr.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Monstr : DestroyPlayer {
+    float dt = 0;
+
+    new void OnTriggerEnter2D(Collider2D col)
+    {
+        Prover(col);
+    }
+
+    void Update()
+    {
+        dt += Time.deltaTime;
+        transform.Translate (Mathf.Cos(dt) * Time.deltaTime, Mathf.Sin(dt)*Time.deltaTime , 0);
+    }
+}
